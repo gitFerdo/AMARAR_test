386b8e8dde0ec35c72ff42d0fcb4c907a70ce0fe
Amasha <it21227004@my.sliit.lk>
2024-01-10 09:21:10 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ import React from 'react'
 function test ()
 {
     return (
-        <div>test 02 test 03 04 05 final</div>
+        <div>
+            <div>test final</div>
+        </div>
     )
 }
 
